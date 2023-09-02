cfb9592e8b8dd41aec37d3b736d4aae4669cf360
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2023-09-02 15:22:21 +0100

    init basic ejs webpage with template to do items

---
+++ public/js/uiUpdates.js
@@ -0,0 +1,5 @@
+function generateRandomNumber(){
+    var randomNumber = Math.floor(Math.random() * 10);
+    console.log(randomNumber); // To check in the browser's console
+    return randomNumber;
+}
